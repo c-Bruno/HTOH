f077cb85555f4b2e00d496c09d10f29d2b5afced
c-Bruno <ribeiro.caboclo@gmail.com>
2020-04-14 23:48:10 -0300

    Criação da tela Splash
    Criado tela splash porem ela ainda não esta funcional.
    Para este commit foi criado apenas o visual da tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HomePageModule } from './home/home.module';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    HomePageModule,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]

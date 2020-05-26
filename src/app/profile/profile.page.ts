c2caf2502f4e160fa5e28a66fa2bd22deea9e134
c-Bruno <ribeiro.caboclo@gmail.com>
2020-05-26 11:49:32 -0300

    inserido a funcionalidade do ion-segment para tela de feed e de perfil do doador

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -7,6 +7,9 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+segment1:boolean=true;
+segment2:boolean=false;
+
 
   constructor(
     public navCtrl: NavController
@@ -17,5 +20,20 @@ export class ProfilePage implements OnInit {
 
   back(){// Redirecionamento para a pagina feed
     this.navCtrl.navigateRoot('/feed'); 
+  }
+
+
+
+  segmentChanged(event){
+    var segment = event.target.value;
+    if(segment == "segment1"){
+      this.segment1 = true;
+      this.segment2 = false;
+    }
+
+    else if(segment == "segment2"){
+      this.segment1 = false;
+      this.segment2 = true;
+    }
   }
 }
